fb6736786f1a4f0d69689bf880714b555a85aa9d
akhDara <90833732+akhDara@users.noreply.github.com>
2022-06-13 01:12:36 +0300

    HomeworkEight

---
+++ HomeworkEight/configuration/ApplicationGlobalState.java
@@ -0,0 +1,37 @@
+package project.configuration;
+
+public final class ApplicationGlobalState {
+
+    private static ApplicationGlobalState INSTANCE;
+    private String selectedCity = null;
+    private final String API_KEY = "NCpzIgzd9LfH7ApfIdqSuU99OaBHfjIJ";
+    private final String DB_FILENAME = "application.db";
+
+    private ApplicationGlobalState() {
+    }
+
+
+    public static ApplicationGlobalState getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ApplicationGlobalState();
+        }
+
+        return INSTANCE;
+    }
+
+    public String getDbFileName() {
+        return DB_FILENAME;
+    }
+
+    public String getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getApiKey() {
+        return this.API_KEY;
+    }
+}
